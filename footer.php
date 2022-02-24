c5df6e197aa50a76c6e13ff6e6ac270d874865b8
Stefan <stepson_jonsson@hotmail.com>
2022-02-24 15:03:33 +0100

    asdasd

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+            echo "<h2>Footer</h2>";
+?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Login</title>
+		<title>Products</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
