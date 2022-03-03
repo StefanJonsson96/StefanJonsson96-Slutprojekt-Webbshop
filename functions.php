88a16b23f4a2f30695311060aeba6e8c2f40dce5
ollkar <72918616+ollkar@users.noreply.github.com>
2022-03-03 09:37:25 +0100

    function admin page added

--- functions.php
+++ functions.php
@@ -256,5 +256,123 @@ function orderDetails($orderid, $produktid, $antal){
     $connection->query("INSERT into orderdetails (OrdersID, ProduktID, Antal) VALUES ($orderid, $produktid, $antal)");
     $connection->close();      
 }
+
+function showProducts()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "Sinus";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT ProduktID, Färg, Kvantitet, Bild, Pris, Kategori FROM produkt  ORDER BY ProduktID ASC";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+  echo "</td><td>" . $row["ProduktID"] . "</td><td>" . $row["Färg"] . "</td><td>"
+  . $row["Kvantitet"]. "</td><td>" .  "<img src=\'data:image/jpg;charset=utf8;base64," 
+  . base64_encode($row['Bild']) . ">" . "</td><td>" . $row["Pris"] 
+  . "</td><td>" . $row["Kategori"] . "</td><td>" . "</td></tr>";
+  }
+  echo "</table>";
+  } 
+  else { echo "0 results"; }
+
+  $conn->close();
+}
+
+function editProducts()
+{
+  if($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Sinus";
+  
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+  
+     $id = $_POST['produktid'];
+     $färg = $_POST['färg'];
+     $kvantitet = $_POST['kvantitet'];
+     $bild = $_POST['bild'];
+     $pris = $_POST['pris'];
+     $kategori = $_POST['kategori'];
+  
+  
+    $statement = $conn->prepare("UPDATE produkt SET Färg=?, Kvantitet=?, Bild=?, Pris=?, Kategori=? WHERE ProduktID=?");
+    $statement->bind_param("sibisi", $färg, $kvantitet, $bild, $pris, $kategori, $id);
+    $statement->execute();
+  
+  
+    if ($statement->execute() === TRUE) {
+          return "Record updated successfully";
+    } 
+    else {
+      return "Error updating: " . $conn->error;
+    }
+
+    /* close statement */
+    mysqli_stmt_close($statement);
+      
+    /* close connection */
+    mysqli_close($conn);
+  
+  }
+}
+
+
+function showOrders()
+{
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "Sinus";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT o.OrdersID, Namn, Gatuadress, Postnr, Stad, Telefon, Orderdatum, od.ProduktID, Färg, Antal, Pris, Bild, Kategori
+  FROM kund
+  INNER JOIN orders AS o ON o.KundID = kund.KundID
+  INNER JOIN orderdetails AS od ON od.OrdersID = o.OrdersID
+  INNER JOIN produkt AS p ON p.ProduktID = od.ProduktID
+  ORDER BY kund.KundID ASC";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+  echo "</td><td>" . $row["OrdersID"] . "</td><td>" . $row["Namn"] . "</td><td>"
+  . $row["Gatuadress"] . "</td><td>" . $row["Postnr"] . "</td><td>" . $row["Stad"] .
+  "</td><td>" . $row["Telefon"] . "</td><td>" . $row["Orderdatum"] .
+  "</td><td>" . $row["ProduktID"] . "</td><td>" . $row["Färg"] .
+  "</td><td>" . $row["Antal"] . "</td><td>" . $row["Pris"] . "</td><td>" . $row["Kategori"] . "</td></tr>";
+  }
+  echo "</table>";
+  } 
+  else { echo "0 results"; }
+
+  $conn->close();
+
+
+}
+
+
+
 ?>
 >>>>>>> Stashed changes
+
+
